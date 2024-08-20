0a56413f4bbc8b9bda60fcea47bc95682c948e09
artemscine <artempemine@gmail.com>
2024-08-20 13:28:30 +0300

    sapphire: Update from V816.0.5.0.UNGMIXM
    Signed-off-by: artemscine <artempemine@gmail.com>

--- proprietary/vendor/bin/init.qti.chg_policy.sh
+++ proprietary/vendor/bin/init.qti.chg_policy.sh
@@ -11,8 +11,7 @@
 export PATH=/vendor/bin
 
 soc_id=`getprop ro.vendor.qti.soc_id`
-if [ "$soc_id" -eq 518 ] || [ "$soc_id" -eq 415 ] || [ "$soc_id" -eq 439 ] || [ "$soc_id" -eq 450 ] || [ "$soc_id" -eq 475 ] || [ "$soc_id" -eq 515 ]; then
-    echo "ro.vendor.qti.soc_id = 518 is here"
+if [ "$soc_id" -eq 415 ] || [ "$soc_id" -eq 439 ] || [ "$soc_id" -eq 450 ] || [ "$soc_id" -eq 475 ] || [ "$soc_id" -eq 515 ]; then
     setprop persist.vendor.hvdcp_opti.start 2
     exit 0
 fi
--- proprietary/vendor/bin/init.qti.media.sh
+++ proprietary/vendor/bin/init.qti.media.sh
@@ -136,7 +136,7 @@ case "$target" in
         esac
         ;;
     "bengal")
-        setprop vendor.mm.target.enable.qcom_parser 12500207
+        setprop vendor.mm.target.enable.qcom_parser 16711407
         case "$soc_hwid" in
             586)
                 setprop vendor.media.target_variant "_khaje_iot"
